f0208d7b0e42f36f15aa4a14d76838233cef0768
Anton Nikolaev <r.ferius@gmail.com>
2020-08-03 11:26:25 +0500

    Создан первый исполняемый скрипт, который выводит список построчно, в стандарный вывод(консоль)

--- examples/05_basic_scripts/access_template.py
+++ examples/05_basic_scripts/access_template.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python3
+
+###
+Сначала элементы списка объединяются в строку, которая разделена символом \n, а в строку
+подставляется номер VLAN, используя форматирование строк.
+###
 
 access_template = ['switchport mode access',
-                   'switchport access vlan {}',
+                  'switchport access vlan {}',
                    'switchport nonegotiate',
                    'spanning-tree portfast',
-                   'spanning-tree bpduguard enable']
+                  'spanning-tree bpduguard enable']
 
 print('\n'.join(access_template).format(5))
