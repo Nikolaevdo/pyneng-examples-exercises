0a7d3aa71dd75828b9106c1011cec5be0f6d7402
Anton Nikolaev <r.ferius@gmail.com>
2020-08-03 12:20:14 +0500

    Базовый скрипт который позволяет задавать параметры через выполнения скрипта

--- examples/05_basic_scripts/access_template_argv.py
+++ examples/05_basic_scripts/access_template_argv.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
+###
+#Этот скрипт позволяет в качестве аргумента параметризировать выполение скрипта.
+#python access_template_argv.py Gi0/7 4
+#где Gi0/7 конкретный порт, а 4 номер VLAN'a
+###
+
 from sys import argv
 
 interface = argv[1]
+
 vlan = argv[2]
-# второй вариант:
-# interface, vlan = argv[1:3]
 
 access_template = ['switchport mode access',
                    'switchport access vlan {}',
@@ -11,5 +18,8 @@ access_template = ['switchport mode access',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable']
 
+
 print('interface {}'.format(interface))
+
 print('\n'.join(access_template).format(vlan))
+
